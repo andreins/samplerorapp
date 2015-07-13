909028acdc5d78e846067c028a1d4b8fd0e4c3a3
Andrei Nae <andreins@ymail.com>
2015-07-13 16:31:49 +0100

    Done user model testing and finished user DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  before_save { self.email = email.downcase }
-  
+  VALID_USERNAME = /\A[A-Za-z]{1}[A-Za-z0-9]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  before_save {  self.email = email.downcase  }
+  validates :username, presence: true, length: {  maximum: 55  },
+                       format: { with: VALID_USERNAME }, uniqueness: { case_sensititve: false }
+
+  validates :email, presence: true, length: { maximum: 255}, uniqueness: { case_sensitive: true },
+                    format: { with: VALID_EMAIL_REGEX }
+
 
 
   devise :database_authenticatable, :registerable, :rememberable, :authentication_keys => [:username]
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,41 +4,41 @@ RSpec.describe User, type: :model do
 
   context 'when creating a new invalid user' do
     it 'should fail if the name is blank' do
-      user = User.new(username:'    ', email:'sam@kcl.ac.uk',
-                      password:'qertywu67', password_confirmation:'qertywu67')
-      expect(user.valid?).to eq(false)
+      user = User.new(username:'    ', email:'sam@kcl.ac.uk')
+            expect(user.valid?).to eq(false)
     end
 
     it 'should fail if the name is too long (more than 55 chars)' do
-      user = User.new(username:'qwertyuiopoiuytrewqwertyuiopoiuytrewqwertyuiopoiuytrewqasdfghjklpoiuytrewqasdfghjkl', email:'sam@kcl.ac.uk',
-                      password:'qertywu67', password_confirmation: 'qertywu67')
+      user = User.new(username:'qwertyuiopoiuytrewqwertyuiopoiuytrewqwertyuiopoiuytrewqasdfghjklpoiuytrewqasdfghjkl',
+                      email:'sam@kcl.ac.uk')
       expect(user.valid?).to eq(false)
     end
 
     it 'should fail if email is invalid format' do
-      user = User.new(username:'kylexx', email:'sam.uk',
-                      password:'qertywu67', password_confirmation: 'qertywu67')
+      user = User.new(username:'kylexx', email:'sam.uk')
       expect(user.valid?).to eq(false)
     end
 
-    it 'should fail if passwords dont match' do
-      user = User.new(username:'faure', email:'sam@kcl.ac.uk',
-                      password:'qertyw2222', password_confirmation:'qertywu67')
-      expect(user.valid?).to eq(false)
+    it 'should fail if the username is not unique' do
+      user1 = User.new(username: 'test', email:'test@yahoo.com')
+      user2 = User.new(username: 'test', email: 'test2@yahoo.com')
+      user1.save!
+      expect(user2.valid?).to eq(false)
     end
 
-    it 'should fail if password is too short' do
-      user = User.new(username:'andyxxx', email:'sam@kcl.ac.uk',
-                      password:'qert', password_confirmation:'qert')
-      expect(user.valid?).to eq(false)
+    it 'should fail if the email is not unique' do
+      user1 = User.new(username: 'test1', email: 'test@test.com')
+      user2 = User.new(username: 'test2', email: 'test@test.com')
+      user1.save!
+      expect(user2.valid?).to eq(false)
     end
+
   end
 
   context 'when creating a new valid user' do
     it 'should succed with valid parameters' do
-      user = User.new(username:'Samm', email:'sam@kcl.ac.uk',
-                      password:'qertywu67', password_confirmation:'qertywu67')
-      expect(user.valid?).to eq(false)
+      user = User.new(username:'Samm', email:'sam@kcl.ac.uk')
+      expect(user.valid?).to eq(true)
     end
   end
 
